6232ccb61efcae0aa3f62fc13beeff63c94dbd67
Ollie Poole <oliverpoole7@gmail.com>
2016-09-17 17:44:23 +0000

    Added red lights for harshness

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ LED_RED_THREE = 20
 LED_GREEN_THREE = 21
 LED_BLUE_THREE = 22
 
+ZAP_ONE = 2
+ZAP_TWO = 3
+
 print("LED - Setup complete")
 
 app = Flask(__name__)
@@ -34,10 +37,20 @@ def reset():
     GPIO.setup(LED_GREEN_THREE, GPIO.OUT, initial= GPIO.LOW)
     GPIO.setup(LED_BLUE_THREE, GPIO.OUT, initial= GPIO.LOW)
 
+    GPIO.setup(ZAP_ONE, GPIO.OUT, initial= GPIO.LOW)
+    GPIO.setup(ZAP_TWO, GPIO.OUT, initial= GPIO.LOW)
+
 @app.route('/')
 def index():
     reset()
     return "Hello World"
+
+@app.route('/zap')
+def zap():
+    GPIO.output(ZAP_ONE, GPIO.HIGH)
+    GPIO.output(ZAP_TWO, GPIO.HIGH)
+
+    return "Zap complete"
 
 @app.route('/start')
 def start():
@@ -49,6 +62,50 @@ def start():
     
     GPIO.output(LED_GREEN_THREE, GPIO.HIGH)
     time.sleep(2)
+
+    return "start-complete"
+
+@app.route('/mild')
+def mild():
+    reset()
+
+    GPIO.output(LED_RED_ONE, GPIO.HIGH)
+    time.sleep(2)
+    
+    reset()
+
+    return "mild-shock-complete"
+
+@app.route('/medium')
+def medium():
+    reset()
+  
+    GPIO.output(LED_RED_ONE, GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(LED_RED_TWO, GPIO.HIGH)
+    time.sleep(2)
+
+    reset()
+
+    return "medium-shock-complete"
+
+@app.route('/harsh')
+def harsh():
+    reset()
+
+    GPIO.output(LED_RED_ONE, GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(LED_RED_TWO, GPIO.HIGH)
+    time.sleep(2)
+
+    GPIO.output(LED_RED_THREE, GPIO.HIGH)
+    time.sleep(2)
+
+    reset()
+
+    return "harsh-shock-complete"
 
 @app.route('/red')
 def red():
